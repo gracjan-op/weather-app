fa0756bdc4be2f7fe67ce0acea7389741a160507
gracjan-op <gropiela@gmail.com>
2025-08-29 15:35:47 +0200

    feat: added configuration for deployment on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   build: {
     outDir: 'dist',
   },
